3cb3a1922a57dd65d80826eb5812e5abe84aa098
lakhdeep23 <lakhdeepsingh23@yahoo.com>
2016-04-28 12:22:46 -0700

    Added files via upload

---
+++ Alien_Shoot/lakhdeepS.cpp
@@ -0,0 +1,31 @@
+//Lakhdeep Singh
+//Lab 5
+//CS 335 - Project
+
+#include <iostream>
+#include <functional>
+using namespace std;
+
+void moveAlien()
+{
+    //move alien...
+    int addgrav = 1;
+    //Update position
+    alien.pos[0] += alien.vel[0];
+    alien.pos[1] += alien.vel[1];
+    //Check for collision with window edges
+    if ((alien.pos[0] < -140.0 && alien.vel[0] < 0.0) ||
+        (alien.pos[0] >= (float)xres+140.0 && alien.vel[0] > 0.0)) {
+    alien.vel[0] = -alien.vel[0];
+    addgrav = 0;
+    }
+    if ((alien.pos[1] < 150.0 && bigfoot.vel[1] < 0.0) ||
+        (alien.pos[1] >= (float)yres && alien.vel[1] > 0.0)) {
+    alien.vel[1] = -alien.vel[1];
+    addgrav = 0;
+    }
+    //Gravity
+    if (addgrav)
+    alien.vel[1] -= 0.75;
+}
+
