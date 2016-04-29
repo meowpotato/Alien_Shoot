1322e70fdb8c2d8ccdc71f4420a785ff54f4a9bc
student <student@lab-315-030.cs.csubak.edu>
2016-04-29 13:36:44 -0700

    Added comments to jeffC.h explaining the purpose of it

--- Alien_Shoot/jeffC.h
+++ Alien_Shoot/jeffC.h
@@ -1,3 +1,13 @@
+/*
+ * Name: Jeff Cadena
+ * CMPS 335 Group Project
+ * Purpose: To create wrapper functions that will use
+ * the frameworks to enable the creation of more weapon choices 
+ * for the user
+ *
+ * */
+
+
 #ifndef JEFFC_H
 #define JEFFC_H
 #endif
