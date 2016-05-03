dfcb04632ecc9f669195f87484248f4e13871bd8
lakhdeep23 <lakhdeepsingh23@yahoo.com>
2016-05-03 13:36:24 -0700

    Added files via upload

---
+++ Alien_Shoot/lakhdeepS.h
@@ -0,0 +1,12 @@
+/*
+ * Author: Lakhdeep Singh
+ * CS 335 Group Project
+ * Purpose: To have definitions. The frameworks for the movement
+ *
+ * */
+
+#ifndef LAKHDEEPS_H
+#define LAKHDEEPS_H
+#endif
+
+void moveAlien()
