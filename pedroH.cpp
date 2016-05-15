16d321d390a7cd7ea519c8b8ff002fc6952aa2c5
globalvariables <phernandez9@csub.edu>
2016-05-14 18:31:02 -0700

    Add files via upload

---
+++ pedroH.cpp
@@ -0,0 +1,12 @@
+/* Author: Pedro 
+ * HW3 - CS 335 Individual Contribution to project
+ * Date Written: 4/15/16-5/3/16, modified 4/27/16, modified 5/2/16
+ * file placeholder
+ * */
+#include <iostream>
+#include <functional>
+#include "common.h"
+
+using namespace std;
+
+
---
+++ pedroH.h
@@ -0,0 +1,10 @@
+/*
+ * Pedro Hernandez 
+ * CS 335 Group Project
+ * Purpose: To have definitions. sounds
+ *
+ * */
+
+
+
+
