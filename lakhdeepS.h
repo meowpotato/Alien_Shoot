61071b7af2d9ebbc4dacc58ded5a9280d03b7fab
lakhdeep23 <lakhdeepsingh23@yahoo.com>
2016-05-31 18:40:41 -0700

    Updated libraries

---
+++ lakhdeepS.h
@@ -0,0 +1,44 @@
+/*
+ * Author: Lakhdeep Singh
+ * CS 335 Group Project
+ * Purpose: To have definitions. The frameworks for the movement
+ *
+ * */
+#include <iostream>
+#include <functional>
+//#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <cstring>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include <cstdlib>
+extern "C"  {
+	#include "fonts.h"
+}
+
+#ifndef LAKHDEEPS_H
+#define LAKHDEEPS_H
+#endif
+
+typedef double Vec[3];
+
+typedef struct t_alien {
+        Vec pos;
+        Vec vel;
+        struct t_alien *prev;
+        struct t_alien *next;
+} Alien;
+
+typedef struct t_human {
+	Vec pos;
+	Vec vel;
+	struct t_human *prev;
+	struct t_human *next;
+} Human;
+
+void moveAlien(Alien);
+void moveHuman(Human);
